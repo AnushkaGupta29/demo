2d2bb72e5d2596d0dcd10f4585a79c38efe49be8
Anushkagupta29 <anushka.gupta2901@gmail.com>
2024-08-14 19:53:32 +0530

    mathematicaloperator

---
+++ nodeJSAssignment/mathematicalOperator.js
@@ -0,0 +1,24 @@
+var a = 5
+console.log(a ** a)
+// secondProblem
+var a = 10, b = 20, c = 15, d = 3, e = 4
+var result = (a + b) * c / d - e
+console.log(result)
+// thirdProblem
+var a = 10, b = 5
+a = a + b
+b = a - b
+a = a - b
+console.log(a, b)
+// fourthProblem
+var C = 110
+F = (C * 9 / 5) + 32
+console.log(F)
+// fifthProblem
+var l = 8, w = 5
+area = l * w 
+console.log(area)
+// sixthProblem
+var a=22
+var result= a%5==0
+console.log(result)
