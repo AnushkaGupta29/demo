7554fa385a66536021b46d897c52c118b48a6f84
Anushkagupta29 <anushka.gupta2901@gmail.com>
2024-08-14 19:53:04 +0530

    conditionalstatement

---
+++ nodeJSAssignment/conditionalStatament.js
@@ -0,0 +1,38 @@
+// firstProgram
+var a = 15
+var b = 10
+if (a > b) {
+    console.log("a is greater than b")
+}
+// secondProgram
+var age = 12
+if (age < 18) {
+    console.log("You are minor")
+}
+else {
+    console.log("You are an adult")
+}
+// thirdProgram
+var number = 125
+if (number % 5 == 0) {
+    console.log("Divisible by 5")
+}
+else {
+    console.log("Not divisible by 5")
+}
+// fourthProgram
+var Num = 23
+if (Num % 2 == 0) {
+    console.log("Number is even")
+}
+else {
+    console.log("Number is odd")
+}
+// fifthProgram
+var num = -45
+if (num > 0) {
+    console.log("Number is Positive")
+}
+else {
+    console.log("Number is Negative")
+}
